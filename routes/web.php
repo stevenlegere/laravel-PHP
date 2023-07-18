dde86776a953d612e267416547a3dd85ea6cb684
stevenlegere <stevenmarklegere@gmail.com>
2023-07-18 15:05:34 +0100

    Created auth login, working on CRUD functionaility, Built table that accepts squat count and date

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\SquatsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/squats', [SquatsController::class, 'index'])->name('squats.index');
+    Route::post('/squats', [SquatsController::class, 'store'])->name('squats.store');
+    Route::put('/squats/{squat}', [SquatsController::class, 'update'])->name('squats.update');
+
+});
+
+require __DIR__.'/auth.php';
