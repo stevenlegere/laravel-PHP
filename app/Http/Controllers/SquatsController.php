e675193f890c876b616aa5dd0928165f68566e61
stevenlegere <stevenmarklegere@gmail.com>
2023-07-18 15:29:46 +0100

    adding Views and controller methods

--- app/Http/Controllers/SquatsController.php
+++ app/Http/Controllers/SquatsController.php
@@ -2,9 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Squat;
 use Illuminate\Http\Request;
 
 class SquatsController extends Controller
 {
     //
+    public function index()
+    {
+        $squats = Squat::all();
+        return view('squats.index', compact('squats'));
+    }
+
+    public function store(Request $request)
+    {
+        Squat::create([
+            'date' => now()->toDateString(),
+            'kilometers' => 0,
+        ]);
+
+        return redirect()->route('squats.index');
+    }
+
+    public function update(Request $request, Squat $squat)
+    {
+        if ($request->has('increment')) {
+            $squat->increment('kilometers', 1);
+        } elseif ($request->has('decrement')) {
+            $squat->decrement('kilometers', 1);
+        }
+
+        return redirect()->route('squats.index');
+    }
+
 }
